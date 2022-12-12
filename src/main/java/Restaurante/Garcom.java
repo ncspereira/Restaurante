c166b90f2d6bf02a4f475782013a5f13ff4718fb
isanatalia <nataliamagalhaesd@gmail.com>
2022-12-12 13:59:31 -0300

    Second release. Missing to fix the getInstance and everything after TelaCliente.

--- src/main/java/Restaurante/Garcom.java
+++
@@ -1,8 +0,0 @@
-package Restaurante;
-
-public class Garcom {
-     //Permitir o cadastro de um item no Menu - ok
-    //Listar o Menu para o cliente - ok
-    //Permitir cadastro de um garçom
-    //Permitir pedir um prato pelo Menu
-}
